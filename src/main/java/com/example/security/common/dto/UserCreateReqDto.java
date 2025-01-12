2c3d0adce70e0b2fef816b92bcc9ce0d66cf59e3
SunMi <wywudi@naver.com>
2025-01-12 15:53:36 +0000

    feat: UserDto 유효성 인증 로직 추가

--- src/main/java/com/example/security/common/dto/UserCreateReqDto.java
+++ src/main/java/com/example/security/common/dto/UserCreateReqDto.java
@@ -1,35 +1,26 @@
-package com.example.security.dto;
+package com.example.security.common.dto;
 
-import com.example.security.Entity.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class UserData {
+public class UserCreateReqDto {
 
-    @Size(min = 3, message = "3~5자리로 입력해주세요")
+    @Size(min = 2, message = "이름은 2자리 이상 입력해주세요")
     @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name;
 
-    @Size(min = 8,max=16, message = "4~8자리로 입력해주세요")
+    @Size(min = 8, max = 16, message = "비밀번호는 8~16자리로 입력해주세요")
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 
     @Email
     @NotBlank(message = "이메일은 필수 입력값입니다.")
     private String email;
-
-    public User toUserEntity(){
-        return User.builder().name(name).password(password).email(email).build();
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
