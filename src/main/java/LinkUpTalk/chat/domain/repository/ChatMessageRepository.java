55b1f1d8945e0669db3ec79c9a6f46b742991954
SunMi <wywudi@naver.com>
2025-01-30 16:23:25 +0000

    feat : 채팅 내역 조회 API

--- src/main/java/LinkUpTalk/chat/domain/repository/ChatMessageRepository.java
+++ src/main/java/LinkUpTalk/chat/domain/repository/ChatMessageRepository.java
@@ -1,11 +1,14 @@
 package LinkUpTalk.chat.domain.repository;
 
 import LinkUpTalk.chat.domain.ChatMessage;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ChatMessageRepository {
     ChatMessage save(ChatMessage chatMessage);
-    List<ChatMessage> findChatMessageByRoomId(Long roomId);
+
+    @Query(value = "{ 'roomId':?0}", sort = "{createdAt: -1}")
+    List<ChatMessage> findTop100ByRoomIdOrderByCreatedAtDesc(Long roomId);
 }
