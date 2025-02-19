6dbb3cf8823f19132bc5cb71ffc39900d39c3ec5
SunMi <wywudi@naver.com>
2025-02-19 12:20:18 +0900

    refactor : setup,cleanup sql 스크립트 분리

--- src/test/resources/websocket-test-setup.sql
+++ src/test/resources/websocket-test-setup.sql
@@ -1,9 +1,9 @@
-INSERT INTO users(id,name,email,password,deleted)
+INSERT INTO users(id,name,email,password,is_deleted)
 VALUES
     (1,'producer','producer@naver.com','password','f'),
     (2,'receiver','receiver@naver.com','password','f');
 
-INSERT INTO chat_room (name,participant_count,capacity,deleted,chat_room_type)
+INSERT INTO chat_room (name,participant_count,capacity,is_deleted,chat_room_type)
 VALUES
     ('TEST GROUP CHAT_ROOM',0,8,'f','GROUP'),
     ('TEST DM CHAT_ROOM',0,2,'f','DM')
