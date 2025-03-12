14a59b9f8203b04af70a8093a7234c4a0bc43d09
SunMi <wywudi@naver.com>
2025-03-12 10:12:27 +0900

    test: unit,integration @tag 어노테이션 분리

--- src/main/java/LinkUpTalk/chat/presentation/ChatController.java
+++ src/main/java/LinkUpTalk/chat/presentation/ChatController.java
@@ -3,6 +3,7 @@ package LinkUpTalk.chat.presentation;
 import LinkUpTalk.chat.application.ChatService;
 import LinkUpTalk.chat.presentation.dto.ChatMessageDmSendReqDto;
 import LinkUpTalk.chat.presentation.dto.ChatMessageReqDto;
+import LinkUpTalk.chat.presentation.event.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -19,9 +20,7 @@ import java.security.Principal;
 public class ChatController {
 
     private final ChatService chatService;
-    private final RedisTemplate<String, Object> redisTemplate;
-    private static final String CHANNEL_TOPIC = "chatroom";
-
+    private final RedisPublisher publisher;
 
     @MessageMapping(value = "/chat/group/{roomId}")
     public void message(@Payload String message,
@@ -29,8 +28,7 @@ public class ChatController {
                         Principal principal) {
         log.info("webSocket Send : {} 사용자가 {}그룹 채팅방에 메시지를 전송",principal.getName(),roomId);
 
-        ChatMessageReqDto groupMessage = chatService.saveGroupMessage(principal.getName(), roomId, message);
-        redisTemplate.convertAndSend(CHANNEL_TOPIC,groupMessage);
+        publisher.sendMessage(chatService.saveGroupMessage(principal.getName(), roomId, message));
     }
 
     @MessageMapping("/chat/dm/{roomId}")
@@ -39,7 +37,6 @@ public class ChatController {
                              Principal principal) {
         log.info("webSocket Send : {} 사용자가 {}개인 채팅방에 메시지를 전송",principal.getName(),roomId);
 
-        ChatMessageReqDto dmMessage=chatService.saveDmMessage(principal.getName(),roomId,req);
-        redisTemplate.convertAndSend(CHANNEL_TOPIC, dmMessage);
+        publisher.sendMessage(chatService.saveDmMessage(principal.getName(),roomId,req));
     }
 }
--- src/test/java/LinkUpTalk/chat/presentation/ChatRoomControllerTest.java
+++ src/test/java/LinkUpTalk/chat/presentation/ChatRoomControllerTest.java
@@ -1,6 +1,6 @@
 package LinkUpTalk.chat.presentation;
 
-import LinkUpTalk.auth.infrastructor.security.config.SecurityConfig;
+import LinkUpTalk.annotation.UnitTest;
 import LinkUpTalk.chat.application.ChatRoomService;
 import LinkUpTalk.chat.config.TestSecurityConfig;
 import LinkUpTalk.chat.config.TestWebSocketConfig;
@@ -8,12 +8,9 @@ import LinkUpTalk.chat.presentation.dto.ChatRoomCreateReqDto;
 import LinkUpTalk.chat.presentation.dto.ChatRoomModifyReqDto;
 import LinkUpTalk.chat.presentation.dto.ChatroomGetResDto;
 import LinkUpTalk.common.response.ResponseCode;
-import LinkUpTalk.user.domain.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,24 +18,14 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -56,14 +43,13 @@ class ChatRoomControllerTest {
     @MockBean
     private ChatRoomService chatRoomService;
 
-    @Test
+    @UnitTest
     @WithMockUser
-    @DisplayName("그룹 채팅방 생성")
+    @DisplayName("유효한 그룹 채팅방 생성 요청 시, 채팅방 생성 성공한다.")
     public void createGroupChatRoom() throws Exception {
         //given
         ChatRoomCreateReqDto reqDto = ChatRoomCreateReqDto.builder().name("Test Room").userId(1L).capacity(10).build();
         String requestBody = new ObjectMapper().writeValueAsString(reqDto);
-        doNothing().when(chatRoomService).createGroup(reqDto);
 
         //when & then
         mvc.perform(post("/chat/rooms/group")
@@ -76,47 +62,42 @@ class ChatRoomControllerTest {
 
     }
 
-    @Test
+    @UnitTest
     @WithMockUser
-    @DisplayName("개인 채팅방 생성")
+    @DisplayName("유효한 개인 채팅방 생성 요청 시, 채팅방 생성 성공한다.한다.")
     public void createDmChatRoom() throws Exception {
-        // given
-        Long receiverId = 2L;
-        User userDetail = User.builder().id(1L).email("user@naver.com").build();
 
-        // MockMvc 요청 시 Security Context 포함
-        mvc.perform(post("/chat/rooms/dm/{receiverId}", receiverId)
-                )
+        //when & then
+        mvc.perform(post("/chat/rooms/dm/{receiverId}", 1L))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value(ResponseCode.CHATROOM_CREATE.getMessage()));
 
-        verify(chatRoomService, times(1)).createDm(any(Long.class), any(String.class));
+        verify(chatRoomService, times(1)).createDm(anyLong(), anyString());
     }
 
 
-    @Test
+    @UnitTest
     @WithMockUser
-    @DisplayName("채팅방 단일 조회")
+    @DisplayName("유효한 채팅방 id로 단일 조회 시, 채팅방 정보 반환 성공한다.")
     void getChatRoom_suc() throws Exception {
         //given
-        Long roomId = 1L;
         ChatroomGetResDto dto = ChatroomGetResDto.builder().name("Test ChatRoom").capacity(10).build();
-        when(chatRoomService.getChatRoom(1L)).thenReturn(dto);
+        when(chatRoomService.getChatRoom(anyLong())).thenReturn(dto);
 
         //when & then
-        mvc.perform(get("/chat/rooms/{roomId}", roomId)
+        mvc.perform(get("/chat/rooms/{roomId}", anyLong())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value(ResponseCode.CHATROOM_READ.getMessage()))
                 .andExpect(jsonPath("$.data.name").value("Test ChatRoom"))
                 .andExpect(jsonPath("$.data.capacity").value(10));
 
-        verify(chatRoomService, times(1)).getChatRoom(roomId);
+        verify(chatRoomService, times(1)).getChatRoom(anyLong());
     }
 
-    @Test
+    @UnitTest
     @WithMockUser
-    @DisplayName("채팅방 리스트 조회")
+    @DisplayName("키워드 검색 조건으로 채팅방 리스트 조회 시, 검색 결과 반환 성공한다.")
     void getChatRooms_suc() throws Exception {
         //given
         List<ChatroomGetResDto> response = List.of(
@@ -133,44 +114,55 @@ class ChatRoomControllerTest {
                 .andExpect(jsonPath("$.message").value(ResponseCode.CHATROOM_LIST_READ.getMessage()))
                 .andExpect(jsonPath("$.data[0].name").value("room 1"))
                 .andExpect(jsonPath("$.data[1].name").value("room 2"));
-
-        verify(chatRoomService, times(1)).getChatRooms(any(Pageable.class), anyString());
     }
 
-    @Test
+    @UnitTest
     @WithMockUser
-    @DisplayName("채팅방 삭제")
-    void deleteChatRoom() throws Exception {
-        //given
-        Long roomId = 1L;
-        doNothing().when(chatRoomService).delete(roomId);
-
+    @DisplayName("유효한 채팅방 id로 참여 유저 리스트 조회 시, 참여 유저 목록 반환 성공한다.")
+    void getUser_suc() throws Exception {
         //when & then
-        mvc.perform(delete("/chat/rooms/{roomId}", roomId)
+        mvc.perform(get("/chat/rooms/{roomId}/participants", anyLong()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(ResponseCode.CHATROOM_PARTICIPANTS_READ.getMessage()));
+    }
+
+    @UnitTest
+    @WithMockUser
+    @DisplayName("유효한 채팅방 id로 메세지 내역 조회 시, 메세지 내역 반환 성공한다.")
+    void getMessageHistories_suc() throws Exception {
+        mvc.perform(get("/chat/rooms/{roomId}/messages", anyLong()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(ResponseCode.CHATROOM_MESSAGE_HISTORY_READ.getMessage()));
+
+    }
+
+    @UnitTest
+    @WithMockUser
+    @DisplayName("유효한 채팅방 ID로 삭제 요청 시, 채팅방 삭제 성공한다.")
+    void deleteChatRoom() throws Exception {
+        //when & then
+        mvc.perform(delete("/chat/rooms/{roomId}", anyLong())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value(ResponseCode.CHATROOM_DELETE.getMessage()));
 
-        verify(chatRoomService, times(1)).delete(roomId);
+        verify(chatRoomService, times(1)).delete(anyLong());
     }
 
-    @Test
+    @UnitTest
     @WithMockUser
-    @DisplayName("채팅방 수정")
+    @DisplayName("유효한 채팅방 수정 요청 시, 채팅방 수정 성공한다.")
     void modifyChatRoom() throws Exception {
         //given
-        Long roomId = 1L;
         ChatRoomModifyReqDto reqDto = ChatRoomModifyReqDto.builder().id(1L).name("Test ChatRoom Modify").capacity(8).build();
-        String reqBody = new ObjectMapper().writeValueAsString(reqDto);
-        doNothing().when(chatRoomService).modify(reqDto, roomId);
 
         //when & then
-        mvc.perform(put("/chat/rooms/{roomId}", roomId)
+        mvc.perform(put("/chat/rooms/{roomId}", 1L)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(reqBody)
+                        .content(new ObjectMapper().writeValueAsString(reqDto))
                 )
                 .andExpect(jsonPath("$.message").value(ResponseCode.CHATROOM_UPDATE.getMessage()));
 
-        verify(chatRoomService, times(1)).modify(any(ChatRoomModifyReqDto.class), eq(1L));
+        verify(chatRoomService, times(1)).modify(refEq(reqDto), eq(1L));
     }
 }
